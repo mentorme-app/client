7acf536740c58bea391265c8afe20c87d7f9aa02
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-24 16:16:47 +0100

    Create user action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,6 @@
 export const SIGNUP_LOAD = "SIGNUP_LOAD";
 export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
+export const USER_LOAD = "USER_LOAD";
+export const USER_SUCCESS = "USER_SUCCESS";
+export const USER_FAILURE = "USER_FAILURE";

ddc479ee5bf05865b9f1d6b8320bfef11a8c568d
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-24 16:16:24 +0100

    Create userProfile action dispatcher

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,13 +1,43 @@
 import axios from "axios";
-import {SIGNUP_LOAD,SIGNUP_SUCCESS } from './actionTypes';
+// import withAuth from "../withAuth";
+import {
+  SIGNUP_LOAD,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE,
+  USER_LOAD,
+  USER_SUCCESS,
+  USER_FAILURE
+} from "./actionTypes";
 
-export const signup  = (name, email, password) => dispatch => {
+export const signup = userData => dispatch => {
+    console.log(userData)
+  const { username, email, password } = userData;
   dispatch({ type: SIGNUP_LOAD });
-   axios
-    .post("https://mentor-me-backend.herokuapp.com/signup", { name, email, password })
-    .then(res => {
-      localStorage.setItem("token", res.data);
-      dispatch({ type: SIGNUP_SUCCESS });
+  axios
+    .post("https://mentor-me-backend.herokuapp.com/api/auth/register", {
+      username,
+      email,
+      password
     })
-    .catch(err => console.log(err.message));
+    .then(res => {
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
+      dispatch({ type: SIGNUP_SUCCESS, payload:res.data.token });
+    })
+    .catch(err => {
+      dispatch({ type: SIGNUP_FAILURE, payload: err.message });
+      console.log(err.message);
+    });
+};
+
+export const userProfile = () => dispatch => {
+  dispatch({ type: USER_LOAD });
+  axios
+    .get("https://mentor-me-backend.herokuapp.com/api/user")
+    .then(res => {
+      dispatch({ type: USER_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: USER_FAILURE, payload: err.message });
+    });
 };

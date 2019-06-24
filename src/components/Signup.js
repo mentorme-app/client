4a1c26dcd7eecad91d34fcd8bada6cef7bd427e3
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-24 11:01:37 +0100

    Create sign up form component
    - pass to parent view container

---
+++ src/components/Signup.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
+import { signup } from "../actions";
+
+const Signup = props => {
+    console.log(props)
+  const [userData, setUser] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+
+  const userSignup = e => {
+    e.preventDefault();
+
+    props.signup(userData)
+  };
+
+  return (
+    <div>
+      <form onSubmit={userSignup}>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={userData.name}
+          onChange={e => setUser({ ...userData, name: e.target.value })}
+        />
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={userData.email}
+          onChange={e => setUser({ ...userData, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={userData.password}
+          onChange={e => setUser({ ...userData, password: e.target.value })}
+        />
+        <button type="submit">
+          {props.signingUp ? (
+            <Loader type="ThreeDots" color="#ccc" height={80} width={80} />
+          ) : (
+            "Sign Up"
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+    signingUp: state.signupReducer.signingUp
+});
+
+export default connect(
+  mapStateToProps,
+  { signup }
+)(Signup);

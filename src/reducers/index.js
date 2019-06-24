b5274b43e6df0c3da69d33fdd196055f020b3a4a
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-24 16:19:54 +0100

    Pass userReducer into the combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import { userReducer } from './userReducer'
 import { signupReducer } from './signupReducer'; 
 
 export default combineReducers({
-    signupReducer
+    signupReducer, userReducer
   });

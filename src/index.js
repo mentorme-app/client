7028f959f29d5cf7dd9f09d4eb0d3bcf7426097c
quansenB <inaki.breinbauer@gmail.com>
2019-06-24 16:09:29 +0200

    Add Router and Routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore, compose } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import rootReducer from './reducers'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from "./reducers";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
-ReactDOM.render(<Provider store={store}>
-    <App />
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
   </Provider>,
-  document.getElementById("root"));
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

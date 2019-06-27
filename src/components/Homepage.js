547c6b2e76283bce99d0b439c4bdda546ca4d9f6
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-27 10:00:42 +0100

    Add search functionality and style home page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import Fuse from "fuse.js";
-import * as styles from "../styled-components/styled-components";
 import { fetchQuestions } from "../actions/actionCreators.js";
-import { searchQuery } from "../actions";
+import {
+  SideNav,
+  BlackLink,
+  StyledHeadSection,
+  StyledH1,
+  StyledHeader,
+  StyledSubheading,
+  StyledSearchBar,
+  StyledLink,
+  StyledQuestionCard,
+  StyledFooter
+} from "../styled-components/styled-components";
 import {
   IoIosSearch,
   IoIosArrowDown,
@@ -14,48 +23,54 @@ import {
 } from "react-icons/io";
 
 function Homepage(props) {
-  const { questions } = props;
+  const { questions, fetchQuestions } = props;
   const [state, toggleMenu] = useState({
     menuBool: false
   });
+  const [search, toggleSearch] = useState({
+    show: false
+  });
 
   const [questionsData, setQuestions] = useState([]);
-  const { fetchQuestions } = props;
 
   useEffect(() => fetchQuestions(), [fetchQuestions]);
   useEffect(() => setQuestions(questions), [questions]);
-  console.log(questions);
-  const [queryText, setQuery] = useState("");
 
-  let options = {
-    keys: ["tag.tag", "author.username"]
-  };
   const SearchHandler = e => {
     e.preventDefault();
-    let fuse = new Fuse(questions, options);
-    let result = fuse.search(e.target.value);
+    const searchQ = e.target.value.toLowerCase();
 
+    const result = questions.filter(question => {
+      const searchValName = question.author.username.toLowerCase();
+      const searchValTag = question.tag.tag.toLowerCase();
+
+      return (
+        searchValName.indexOf(searchQ) !== -1 ||
+        searchValTag.indexOf(searchQ) !== -1
+      );
+    });
     setQuestions(result);
-    setQuery(e.target.value);
   };
 
   return (
     <div>
       {state.menuBool && (
-        <styles.SideNav>
-          <styles.BlackLink to="/home">
+        <SideNav>
+          <BlackLink to="/home">
             <IoIosHome /> Home
-          </styles.BlackLink>
-          <styles.BlackLink to="/chats">
+          </BlackLink>
+
+          <BlackLink to="/chats">
             <IoIosChatbubbles /> Chats
-          </styles.BlackLink>
-          <styles.BlackLink to="/changeProfile">
+          </BlackLink>
+
+          <BlackLink to="/changeProfile">
             <IoIosBuild /> Change Profile
-          </styles.BlackLink>
-        </styles.SideNav>
+          </BlackLink>
+        </SideNav>
       )}
-      <styles.StyledHeadSection>
-        <styles.StyledHeader>
+      <StyledHeadSection>
+        <StyledHeader>
           <div
             onClick={() => {
               toggleMenu({ menuBool: !state.menuBool });
@@ -63,52 +78,55 @@ function Homepage(props) {
           >
             <IoIosMenu />
           </div>
-          <styles.StyledH1>Questions Feed</styles.StyledH1>
-          <styles.BlackLink to="/search">
-            <IoIosSearch />
-          </styles.BlackLink>
-        </styles.StyledHeader>
-        <styles.StyledSubheading>
-          Topic <IoIosArrowDown />
-        </styles.StyledSubheading>
-      </styles.StyledHeadSection>
-      <div>
-        <input
-          type="search"
-          placeholder="Enter item"
-          value={queryText}
-          onChange={SearchHandler}
-        />
-        <input type="submit" value="Search" />
+          <StyledH1>Questions Feed</StyledH1>
 
-        {/* {results > 0 && results.map(result => <div>{result.question}</div>)} */}
-      </div>
+          <IoIosSearch
+            onClick={() => {
+              toggleSearch({ show: !search.show });
+            }}
+          />
+        </StyledHeader>
+        <StyledSubheading>
+          Topic <IoIosArrowDown />
+        </StyledSubheading>
+      </StyledHeadSection>
+
+      {search.show && (
+        <StyledSearchBar>
+          <div>What would you like to help with?</div>
+          <input
+            type="search"
+            placeholder="Search..."
+            onChange={SearchHandler}
+          />
+        </StyledSearchBar>
+      )}
 
       {questionsData.map(question => {
         return (
-          <styles.StyledLink style={question.display && !question.display && { display: "none" }} key={question.id} to={`/question/${question.id}`}>
-            <styles.StyledQuestionCard
+          <StyledLink key={question.id} to={`/question/${question.id}`}>
+            <StyledQuestionCard
               key={question.id}
               image={question.author.avatar}
             >
               <h1>{question.author.username}</h1>
-              <h2>{question.tag.tag}</h2>
-              <p>{question.question}</p>
-            </styles.StyledQuestionCard>
-          </styles.StyledLink>
+              <h3>{question.tag.tag}</h3>
+              <p>{question.title}</p>
+            </StyledQuestionCard>
+          </StyledLink>
         );
       })}
-      <styles.StyledFooter>
-        <styles.BlackLink to="/home">
+      <StyledFooter>
+        <BlackLink to="/home">
           <IoIosHome />
-        </styles.BlackLink>
-        <styles.BlackLink to="/chats">
+        </BlackLink>
+        <BlackLink to="/chats">
           <IoIosChatbubbles />
-        </styles.BlackLink>
-        <styles.BlackLink to="/changeProfile">
+        </BlackLink>
+        <BlackLink to="/changeProfile">
           <IoIosBuild />
-        </styles.BlackLink>
-      </styles.StyledFooter>
+        </BlackLink>
+      </StyledFooter>
     </div>
   );
 }
@@ -119,5 +137,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { fetchQuestions, searchQuery }
+  { fetchQuestions }
 )(Homepage);
